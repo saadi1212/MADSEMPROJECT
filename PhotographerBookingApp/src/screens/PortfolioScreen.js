ec1406fb264e458f72888007f5fc3fa8c9da2ba8
akasha181 <akashaakmal97@gmail.com>
2026-01-01 12:04:29 +0500

    BY AKASHA AKMAL

---
+++ PhotographerBookingApp/src/screens/PortfolioScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function PortfolioScreen() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text>Portfolio Screen</Text>
+      <Text>Images will be shown here</Text>
+    </View>
+  );
+}
